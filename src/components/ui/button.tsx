bdc843e5eeb92d3514b11847abe91773fbbccc93
Barend Briel <barend@encounterchurch.co.za>
2026-04-28 19:42:58 +0200

    Restyle shared app buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -39,7 +39,15 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    if (asChild) {
+      return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    }
+
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        <span className="matrix-button__content">{props.children}</span>
+      </Comp>
+    );
   },
 );
 Button.displayName = "Button";
