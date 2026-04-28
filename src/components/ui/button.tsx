96b20aa0944a7d51bd6410ed127cdc5481faca2e
Barend Briel <barend@encounterchurch.co.za>
2026-04-28 19:49:42 +0200

    Restore original shared button styling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -39,15 +39,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    if (asChild) {
-      return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
-    }
-
-    return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
-        <span className="matrix-button__content">{props.children}</span>
-      </Comp>
-    );
+    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
   },
 );
 Button.displayName = "Button";
